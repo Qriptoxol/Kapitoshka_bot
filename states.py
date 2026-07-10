a7f5509dffeb64fb7e64a61b8310e979622cd0b5
KingDog14 <vasavladarev@gmail.com>
2026-07-10 19:56:08 +1100

    Update states.py

--- states.py
+++ states.py
@@ -1,20 +1,17 @@
 from database import get_state, set_state, clear_state
 
 class StateManager:
-    """Менеджер состояний, хранит в БД."""
-
     @staticmethod
-    async def get(update, context):
+    def get(update):
         user_id = update.effective_user.id
-        state = get_state(user_id)
-        return state
+        return get_state(user_id)
 
     @staticmethod
-    async def set(update, context, state):
+    def set(update, state):
         user_id = update.effective_user.id
         set_state(user_id, state)
 
     @staticmethod
-    async def clear(update, context):
+    def clear(update):
         user_id = update.effective_user.id
         clear_state(user_id)
