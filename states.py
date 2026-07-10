3995322b6d1f9ee6e4cd96c347bbcfd17683b5fd
KingDog14 <vasavladarev@gmail.com>
2026-07-10 20:16:27 +1100

    Update states.py

--- states.py
+++ states.py
@@ -3,15 +3,12 @@ from database import get_state, set_state, clear_state
 class StateManager:
     @staticmethod
     def get(update):
-        user_id = update.effective_user.id
-        return get_state(user_id)
+        return get_state(update.effective_user.id)
 
     @staticmethod
     def set(update, state):
-        user_id = update.effective_user.id
-        set_state(user_id, state)
+        set_state(update.effective_user.id, state)
 
     @staticmethod
     def clear(update):
-        user_id = update.effective_user.id
-        clear_state(user_id)
+        clear_state(update.effective_user.id)
