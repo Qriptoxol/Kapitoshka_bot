da0ede4e7ce4781ef70656664a1df971befc0de1
KingDog14 <vasavladarev@gmail.com>
2026-07-10 19:57:17 +1100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,10 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
-async def send_inactive_warning(bot, user_id):
-    """Отправляет пользователю предупреждение о бездействии."""
+def send_inactive_warning(bot, user_id):
     keyboard = InlineKeyboardMarkup([
         [InlineKeyboardButton("Я здесь!", callback_data=f"i_am_here_{user_id}")]
     ])
-    await bot.send_message(
+    bot.send_message(
         chat_id=user_id,
         text="Потерял интерес, сладенький, может кикнуть?",
         reply_markup=keyboard
